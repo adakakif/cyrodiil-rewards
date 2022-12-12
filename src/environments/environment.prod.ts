81317f3be32c9e9a7a7f84da2a916cf50b913c06
Akif Adak <adakakif@gmail.com>
2022-12-12 08:44:44 +0300

    feat(api): Add firebase without emulators.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBVV3UBA-DL2wOJxfAKhPTrdbqwVKCIiBA",
+    authDomain: "cyrodiil-rewards.firebaseapp.com",
+    projectId: "cyrodiil-rewards",
+    storageBucket: "cyrodiil-rewards.appspot.com",
+    messagingSenderId: "598110197149",
+    appId: "1:598110197149:web:401adb7b4bd57decd8cb74",
+    locationId: 'us-central',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'cyrodiil-rewards',
+    appId: '1:598110197149:web:401adb7b4bd57decd8cb74',
+    storageBucket: 'cyrodiil-rewards.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBVV3UBA-DL2wOJxfAKhPTrdbqwVKCIiBA',
+    authDomain: 'cyrodiil-rewards.firebaseapp.com',
+    messagingSenderId: '598110197149',
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
